f0c94f7cff3789d1ae4a531faa46149bdbde1824
regisds <registr@nate.com>
2017-11-01 10:08:19 +0900

    171101_notebook progress

---
+++ ShootMulti1/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+	public Transform tilePrefab;
+	public Transform obstaclePrefab;
+	public Vector2 mapSize;
+	[Range(0,1)]
+	public float outlinePercent;
+
+	List<Coord> allTileCoords;
+	Queue<Coord> shuffleTileCoords;
+	public int seed = 10;
+	void Start(){
+		GenerateMap();
+	}
+
+
+
+	public void GenerateMap (){
+
+		allTileCoords = new List<Coord> ();
+		for (int x=0;x < mapSize.x; x ++){
+			for (int y=0; y<mapSize.y; y ++){
+				allTileCoords.Add(new Coord(x,y));
+			}
+		}
+
+		shuffleTileCoords = new Queue<Coord> (Utility.ShuffleArray(allTileCoords.ToArray(), seed));
+
+		string holderName = "Generated Map";
+		if (transform.Find (holderName)){
+			DestroyImmediate(transform.Find(holderName).gameObject);
+		}
+
+		Transform mapHolder = new GameObject(holderName).transform;
+		mapHolder.parent = transform;
+
+
+		for (int x=0;x < mapSize.x; x ++){
+			for (int y=0; y<mapSize.y; y ++){
+				Vector3 tilePosition = CoordToPosition(x,y);
+				Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right*90)) as Transform;
+				newTile.localScale = Vector3.one * (1-outlinePercent);
+				newTile.parent = mapHolder;
+			}
+		}
+
+		int obstacleCount = 10;
+		for (int i =0;i<obstacleCount;i++){
+			Coord randomCoord = GetRandomCoord();
+			Vector3 ObstaclePosition = CoordToPosition (randomCoord.x,randomCoord.y);
+			Transform newObstacle = Instantiate (obstaclePrefab, ObstaclePosition, Quaternion.identity) as Transform;
+		}
+
+	}
+
+	Vector3 CoordToPosition (int x, int y) {
+		return new Vector3(-mapSize.x/2 +0.5f + x,0, -mapSize.y/2 + 0.5f + y);
+	}
+
+
+
+
+	public Coord GetRandomCoord (){
+		Coord randomCoord = shuffleTileCoords.Dequeue();
+		shuffleTileCoords.Enqueue(randomCoord);
+		return randomCoord;
+	}
+
+	public struct Coord{
+		public int x;
+		public int y;
+		public Coord(int _x, int _y){
+			x=_x;
+			y=_y;
+		}
+	}		
+}
